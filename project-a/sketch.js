efda717beb07dbb5912855e5d7c9bedf6544abc0
Jessiezhouu0316 <bz2727@nyu.edu>
2025-10-21 14:39:33 +0800

    final

--- project-a/sketch.js
+++ project-a/sketch.js
@@ -7,16 +7,17 @@ let h;
 let p = 80;
 let a = 0.01;
 let b = 0.008;
-let tx = 0;  // 噪声时间变量
-let ty = 1000; // 分开一点避免两个轴同步
+let tx = 0;
+let ty = 1000;
 let d;
 let safeDistance;
 
 function setup() {
-  background(0,0,0,0.0005);
+  background(0, 0, 0, 0.005);
   let c = createCanvas(800, 500);
   c.parent('p5-canvas-container');
   colorMode(HSB);
+
   x = random(width);
   y = random(height);
   s = random(80, 100);
@@ -30,7 +31,6 @@ function setup() {
     beginShape();
     for (let angle = 0; angle < 2 * PI; angle += PI / 30) {
       let n = noise(cos(angle + b) * 0.5, sin(angle * b) * 0.5);
-
       let a = map(n, 0, 1, -50, 50);
       let x = width / 2 + (b + a) * cos(angle);
       let y = height / 2 + (b + a) * sin(angle);
@@ -44,41 +44,41 @@ function setup() {
 }
 
 function draw() {
-  background(0,0,100,0.01);
+  background(0, 0, 100, 0.01);
   fill(h, 80, p, 0.3);
   noStroke();
+
+  //Breathing movement
   s = map(sin(frameCount * 0.1), -1, 1, 10, 30);
   Moss(x, y, s);
 
-  //noise update
+  //Update noise value
   tx += a;
   ty += b;
 
   let noiseTargetX = width * noise(tx);
   let noiseTargetY = height * noise(ty);
 
+  //Calculate the distance
   d = dist(x, y, mouseX, mouseY);
   safeDistance = 150;
 
   if (d < safeDistance) {
+    let dx = x - mouseX;
+    let dy = y - mouseY;
 
-    let finalTargetX = mouseX + safeDistance;
-    let finalTargetY = mouseY + safeDistance;
-
-    // 平滑移动到目标点（越近越快）
+    //Move to the target point smoothly
     let moveSpeed = map(d, 0, safeDistance, 0.3, 0.1);
-    x = lerp(x, finalTargetX, moveSpeed);
-    y = lerp(y, finalTargetY, moveSpeed);
+    x = lerp(x, x+dx, moveSpeed);
+    y = lerp(y, y+dy, moveSpeed);
   } else {
-    // 距离够远时，恢复噪声漂浮
     x = lerp(x, noiseTargetX, 0.1);
     y = lerp(y, noiseTargetY, 0.1);
   }
 
-  // 随机色变化
+  //Hue change
   h = 360 * noise(frameCount * 0.01);
 
-  // 鼠标按下时生态受损变黑变慢
   if (mouseIsPressed) {
     p = lerp(p, 0, 0.01);
     a = lerp(a, 0, 0.01);
